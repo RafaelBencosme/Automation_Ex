2d90d66ceac4b5d810dac6812d61841fa502d9ce
Rafael B <rafaelabencosmed@gmail.com>
2024-12-14 01:03:46 -0400

    Added 'New User Registration' Test

--- cypress/e2e/exercise.cy.js
+++ cypress/e2e/exercise.cy.js
@@ -1,44 +1,88 @@
 import TopNavigation from "./Components/TopNavigation";
 import Products from "./Pages/Products";
 import Cart from "./pages/cart";
-import Signup from "./pages/signup";
 import Checkout from "./Pages/Checkout";
 import Payment from "./pages/payment";
+import Signup from "./pages/signup";
 
-describe("Automation_Excercice", () => {
+describe("Automation_Exercise", () => {
+  let topNav, products, cart, signUp, checkout, payment;
+
   beforeEach(() => {
     cy.visit("/");
+    topNav = new TopNavigation();
+    products = new Products();
+    cart = new Cart();
+    checkout = new Checkout();
+    payment = new Payment();
+    signUp = new Signup();
   });
 
-  it("Should be able to add to cart", () => {
-    const topNav = new TopNavigation();
-    topNav.selectProducts();
+  describe("Complete Purchase", () => {
+    it("Should be able to add to cart and log in", () => {
+      topNav.selectProducts();
 
-    const products = new Products();
-    products.selectProduct3();
-    products.product3Information.should("exist");
+      products.selectProduct3();
+      products.product3Information.should("exist");
 
-    products.setRandomQuantity();
-    products.selectAddToCart();
-    products.selectPopupViewCart();
+      products.setRandomQuantity();
+      products.selectAddToCart();
+      products.selectPopupViewCart();
 
-    const cart = new Cart();
-    cart.selectProceedToCheckout();
-    cart.selectModalLoginRegister();
+      cart.selectProceedToCheckout();
+      cart.selectModalLoginRegister();
 
-    const signup = new Signup();
-    signup.login("faker@fakemail.com", "fakepassword");
+      signUp.login("faker@fakemail.com", "fakepassword");
 
-    topNav.selectCart();
-    cart.selectProceedToCheckout();
+      topNav.selectCart();
+      cart.selectProceedToCheckout();
 
-    const checkout = new Checkout();
-    checkout.selectPlaceOrder();
+      checkout.selectPlaceOrder();
 
-    const payment = new Payment();
-    payment.fillPaymentMethod("fake name", "fake card", "cvc", "month", "year");
-    checkout.orderPlaced.should("exist");
-    topNav.selectLogout();
-    signup.loginTitle.should("exist");
+      payment.fillPaymentMethod(
+        "fake name",
+        "fake card",
+        "cvc",
+        "month",
+        "year"
+      );
+      checkout.orderPlaced.should("exist");
+      topNav.selectLogout();
+      signUp.loginTitle.should("exist");
+    });
+
+    it("Should be able to add to cart and create a user", () => {
+      topNav.selectProducts();
+
+      products.selectProduct3();
+      products.product3Information.should("exist");
+
+      products.setRandomQuantity();
+      products.selectAddToCart();
+      products.selectPopupViewCart();
+
+      cart.selectProceedToCheckout();
+      cart.selectModalLoginRegister();
+
+      const signup = new Signup();
+      signup.register();
+      signup.fillNewUserForm();
+      
+      topNav.selectCart();
+      cart.selectProceedToCheckout();
+
+      checkout.selectPlaceOrder();
+
+      payment.fillPaymentMethod(
+        "fake name",
+        "fake card",
+        "cvc",
+        "month",
+        "year"
+      );
+      checkout.orderPlaced.should("exist");
+      topNav.selectLogout();
+      signUp.loginTitle.should("exist");
+    });
   });
 });
--- cypress/e2e/pages/Signup.js
+++ cypress/e2e/pages/Signup.js
@@ -1,13 +1,55 @@
-class Signup {
-    login(email, password) {
-        cy.get('[data-qa="login-email"]').type(email);
-        cy.get('[data-qa="login-password"]').type(password);
-        cy.get('[data-qa="login-button"]').click();
-    }
+import { faker } from "@faker-js/faker";
 
-    get loginTitle() {
-       return cy.get('.login-form > h2').should('exist');
-    }
+class Signup {
+  login(email, password) {
+    cy.get('[data-qa="login-email"]').type(email);
+    cy.get('[data-qa="login-password"]').type(password);
+    cy.get('[data-qa="login-button"]').click();
+  }
+
+  register() {
+    const randomName = faker.person.fullName();
+    const randomEmail = faker.internet.email();
+
+    cy.get('[data-qa="signup-name"]').type(randomName);
+    cy.get('[data-qa="signup-email"]').type(randomEmail);
+    cy.get('[data-qa="signup-button"]').click();
+  }
+
+  fillNewUserForm() {
+    const randomPassword = faker.internet.password();
+    const day = faker.number.int({ min: 1, max: 28 }).toString();
+    const month = faker.date.month();
+    const year = faker.number.int({ min: 1950, max: 2025 }).toString();
+    const randomFirstName = faker.person.firstName();
+    const randomLastName = faker.person.lastName();
+    const randomCompany = faker.person.fullName();
+    const randomAddress = faker.location.streetAddress();
+    const randomState = faker.location.state();
+    const randomCity = faker.location.city();
+    const randomZip = faker.location.zipCode();
+    const randomMobile = faker.phone.number("1+###-###-####");
+
+    cy.get("#id_gender1").click();
+    cy.get('[data-qa="password"]').type(randomPassword);
+    cy.get('[data-qa="days"]').select(day);
+    cy.get('[data-qa="months"]').select(month);
+    cy.get('[data-qa="years"]').select(year);
+    cy.get('[data-qa="first_name"]').type(randomFirstName);
+    cy.get('[data-qa="last_name"]').type(randomLastName);
+    cy.get('[data-qa="company"]').type(randomCompany);
+    cy.get('[data-qa="address"]').type(randomAddress);
+    cy.get('[data-qa="country"]').select("United States");
+    cy.get('[data-qa="state"]').type(randomState);
+    cy.get('[data-qa="city"]').type(randomCity);
+    cy.get('[data-qa="zipcode"]').type(randomZip);
+    cy.get('[data-qa="mobile_number"]').type(randomMobile);
+    cy.get('[data-qa="create-account"]').click();
+  }
+
+  get loginTitle() {
+    return cy.get(".login-form > h2").should("exist");
+  }
 }
 
 export default Signup;
